0138ee07c66d93064f540afd39752bf16a488329
NarsiBhatii <narsibhati31102@gmail.com>
2024-08-03 20:15:59 +0530

    assignment 1 prof of work

---
+++ prof_of_work/assignment1.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto');
+
+function findHashWithPrefix(prefix) {
+    let input = 0; 
+
+    while(true) {
+        let inputStr = input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex'); 
+
+        if(hash.startsWith(prefix)) {
+            return { input: inputStr, hash: hash}
+        }
+        input++;
+    }
+}
+
+const result = findHashWithPrefix('00000');
+console.log(`input : ${result.hash}`);
+console.log(`hash : ${result.hash}`);
