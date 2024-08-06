aa0233c40279c40ef1393bd347bb3b886be92a61
NarsiBhatii <narsibhati31102@gmail.com>
2024-08-06 15:04:25 +0530

    assinment 2 and 3

--- prof_of_work/assignment1.js
+++ prof_of_work/assignment1.js
@@ -1,11 +1,11 @@
-import { createHash } from 'crypto';
+const crypto = require('crypto');
 
 function findHashWithPrefix(prefix) {
     let input = 0; 
 
     while(true) {
         let inputStr = input.toString();
-        let hash = createHash('sha256').update(inputStr).digest('hex'); 
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex'); 
 
         if(hash.startsWith(prefix)) {
             return { input: inputStr, hash: hash}
