2d2b8888ace4582c0d24986a19bffa9610188885
NarsiBhatii <narsibhati31102@gmail.com>
2024-08-08 23:51:05 +0530

    week1

--- prof_of_work/assignment2.js
+++ prof_of_work/assignment2.js
@@ -1,19 +1,19 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-function findHashWithPrefix(startString,Prefix) {
-    let input = 0; 
-    while(true) {
-        let inputStr = startString + input.toString();
-        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+function findHashWithPrefix(startString, Prefix) {
+  let input = 0;
+  while (true) {
+    let inputStr = startString + input.toString();
+    let hash = crypto.createHash("sha256").update(inputStr).digest("hex");
 
-        if(hash.startsWith(Prefix)) {
-            return {input: input , hash: hash};
-        }
-
-        input++;
+    if (hash.startsWith(Prefix)) {
+      return { input: input, hash: hash };
     }
+
+    input++;
+  }
 }
 
-const result = findHashWithPrefix('100xdevs','00000');
-console.log(`input: 100xdevs + ${result.input}`)
-console.log(`hash ${result.hash}`)
+const result = findHashWithPrefix("100xdevs", "00000");
+console.log(`input: 100xdevs + ${result.input}`);
+console.log(`hash ${result.hash}`);
--- prof_of_work/assignment3.js
+++ prof_of_work/assignment3.js
@@ -1,22 +1,21 @@
-const crypto = require('crypto'); 
+const crypto = require("crypto");
 
-function findHashWithPrefix(startWith,prefix) {
-    let input = 0;
-    while(true) {
-        let inputStr = startWith + input.toString();
-        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
-        if(hash.startsWith(prefix)) {
-            return {input: input,hash: hash} 
-        }
-        input++;
+function findHashWithPrefix(startWith, prefix) {
+  let input = 0;
+  while (true) {
+    let inputStr = startWith + input.toString();
+    let hash = crypto.createHash("sha256").update(inputStr).digest("hex");
+    if (hash.startsWith(prefix)) {
+      return { input: input, hash: hash };
     }
+    input++;
+  }
 }
-
 
 const startWith = `
 harkirat => Raman | Rs 100 
 Ram => Ankit | Rs 10
 `;
-const result = findHashWithPrefix(startWith,'00000');
+const result = findHashWithPrefix(startWith, "00000");
 console.log(`input: ${startWith} + ${result.input}`);
 console.log(`hash:  ${result.hash}`);
